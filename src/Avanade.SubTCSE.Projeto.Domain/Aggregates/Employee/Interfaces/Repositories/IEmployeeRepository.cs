03a8a089fddb983ba7ff05d5ac32f4212419bc7d
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2021-07-07 11:50:51 -0300

    BaseRepository, conlusão aula 16

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
@@ -5,6 +5,6 @@ namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Reposito
 {
     public interface IEmployeeRepository : IBaseRepository<Entities.Employee, string>
     {
-        Task<Entities.Employee> AddEmployee(Entities.Employee employee);
+
     }
 }
